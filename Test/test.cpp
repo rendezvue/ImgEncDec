d02b31dbb87d997e59f810f58f369b3bff8c1915
ddorobot <ddorobot@gmail.com>
2019-03-22 23:44:48 -0700

    Check Mem Size(RGB vs YUV420)

--- Test/test.cpp
+++ Test/test.cpp
@@ -15,9 +15,16 @@ int main(void)
 	cv::Mat rgb = cv::imread("test.bmp") ;
 
 	cv::Mat yuv420 = cls_image_encoder.Encoding(rgb);
+	cv::Mat rgb888 = cls_image_decoder.Decoding(yuv420) ;
 	
 	cv::imshow("rgb", rgb) ;
 	cv::imshow("yuv420", yuv420) ;
+	cv::imshow("rgb888", rgb888) ;
+
+	printf("rgb mem size = %d\n", (unsigned int)(rgb.total() * rgb.elemSize())) ;
+	printf("yuv420 mem size = %d\n", (unsigned int)(yuv420.total() * yuv420.elemSize())) ;
+	printf("rgb888 mem size = %d\n", (unsigned int)(rgb888.total() * rgb888.elemSize())) ;
+	
 	cv::waitKey(0) ;
 	
 	return 0 ;
